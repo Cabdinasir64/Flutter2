d2aad89adb29cc5f351e9a34f349878c08368596
Cabdinasir64 <cabdinasira64@gmail.com>
2026-03-23 09:27:36 +0300

    feat: ku dar setup Isar iyo routing screens
    - Initialize garee Isar database
    - Ku xiro schemas (User, Profile, Post, Tag)
    - Samee routes screens kala duwan
    - Ku dar main app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:flutter2/models/user3.dart';
-import 'package:flutter2/models/post1.dart';
-import 'package:flutter2/screens/isar_learning/user_list_screen3.dart';
-import 'package:flutter2/screens/isar_learning/post_manager_screen1.dart';
+import 'package:flutter2/models/scheme1.dart';
+import 'package:flutter2/screens/isar_learning/post_detail_screen.dart';
+import 'package:flutter2/screens/isar_learning/post_list_screen.dart';
+import 'package:flutter2/screens/isar_learning/profile_screen.dart';
+import 'package:flutter2/screens/isar_learning/user_list_screen.dart';
+import 'package:flutter2/screens/isar_learning/tag_explorer_screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
   final dir = await getApplicationDocumentsDirectory();
   
   final isar = await Isar.open(
-    [User3Schema, Post1Schema], 
-    directory: dir.path
+    [UserSchema, ProfileSchema, PostSchema, TagSchema],
+    directory: dir.path,
   );
 
-  runApp(IsarRelationshipApp(isar: isar));
+  runApp(IsarMasterApp(isar: isar));
 }
 
-class IsarRelationshipApp extends StatelessWidget {
+class IsarMasterApp extends StatelessWidget {
   final Isar isar;
-  const IsarRelationshipApp({super.key, required this.isar});
+  const IsarMasterApp({super.key, required this.isar});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Isar Relationships',
       initialRoute: '/',
       routes: {
         '/': (context) => UserListScreen(isar: isar),
-        '/posts': (context) => PostManagerScreen(isar: isar),
+        '/profile': (context) => ProfileScreen(isar: isar),
+        '/posts': (context) => PostListScreen(isar: isar),
+        '/post_detail': (context) => PostDetailScreen(isar: isar),
+        '/tags': (context) => TagExplorerScreen(isar: isar),
       },
     );
   }
