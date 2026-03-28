18428d296b3fff32b8aa171381032c7fe339f67c
Cabdinasir64 <cabdinasira64@gmail.com>
2026-03-28 09:33:22 +0300

    feat: bilaabista Isar database iyo habaynta navigation-ka app-ka

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'models/scheme2.dart';
-import 'package:flutter2/screens/isar_learning/search2.dart';
+import 'package:flutter2/screens/isar_learning/watch_product_screen1.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
   final dir = await getApplicationDocumentsDirectory();
   
   final isar = await Isar.open(
@@ -16,7 +17,7 @@ void main() async {
     debugShowCheckedModeBanner: false,
     initialRoute: '/',
     routes: {
-      '/': (context) => Search2Screen(isar: isar),
+      '/': (context) => WatchProductScreen(isar: isar),
     },
   ));
 }
